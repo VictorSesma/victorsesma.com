d7e33d8015fc62baa04d0bb2883fc71849d3191f
Victor Sesma <leviatan.89@gmail.com>
2022-05-09 22:12:08 +0100

    deleted google tag manager

--- src/App.js
+++ src/App.js
@@ -184,13 +184,6 @@ class CurriculumVitae extends Component {
 }
 
 class Header extends Component {
-	componentDidMount() {
-		const s = document.createElement('script');
-		s.type = 'text/javascript';
-		s.async = true;
-		s.innerHTML = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-54TXN3H')";
-		document.head.appendChild(s);
-	}
 	render() {
 		return (
 			<header className="App-header">
